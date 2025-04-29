d0ce4eb55eee8599cc24fc677e0c3b233cc9f9d2
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-04-29 11:03:49 +0300

    chore: ignore pyright issues

--- player.py
+++ player.py
@@ -21,7 +21,9 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(
+            screen, "white", self.triangle(), 2  # pyright: ignore
+        )
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
