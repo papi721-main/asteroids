d8196b00f5c402d90c9ec796242d695d487cbbc7
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-04-29 08:52:46 +0300

    feat: learn about modules and importing

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import pygame
+
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
